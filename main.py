bb28e709209ba77c67433b6e08d857780cffa11a
Rasmus Skovgaard Pedersen <201908279@post.au.dk>
2023-02-01 12:31:20 +0100

    "Tilføjet malta osm fil"

--- main.py
+++ main.py
@@ -1,8 +1,47 @@
 import xml.etree.ElementTree as ET
 
-if __name__ == '__main__':
+import geopandas as gpd
+
+
+def noteTest():
     tree = ET.parse('Data/malta-latest.osm')
     root = tree.getroot()
+    nodeDict = {
+    }
+
+
+    ##Find nodes with tag = highway
+    for child in root:
+        if child.tag == 'node':
+            for tag in child:
+                if tag.attrib['k'] == 'highway':
+                    nodeDict[child.attrib['id']] = child.attrib['lat'] + ',' + child.attrib['lon']
+                    break
+
+    df_world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    print(f"{type(df_world)}, {df_world.geometry.name}")
+
+def plotTest():
+    df_world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+    cities = gpd.read_file(gpd.datasets.get_path('naturalearth_cities'))
+
+    print(df_world.geometry.geom_type.value_counts())
+    #df_world.plot(figsize=(10, 6))
+    df_world.head()
+    df_world.plot()
+
+
+
+if __name__ == '__main__':
+
+    plotTest()
+
+
+
+
+
+
 
 
 
